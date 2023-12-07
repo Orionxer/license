be33f98570ea0f66d6bb16fe38fc87eb1ba21ce7
Orionxer <orionxer@gmail.com>
2023-12-07 07:34:21 +0000

    ✨(feat): Add More Licenses
    - Add badges in README
    - Add name for docker container
    - Change license to BSD 3-Clause

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # License archives
+![Static Badge](https://img.shields.io/badge/author-Orionxer-green)
+![GitHub License](https://img.shields.io/github/license/orionxer/license)
+![GitHub top language](https://img.shields.io/github/languages/top/orionxer/license)
 
 ## Docker environment
 ### Dokcer compose
--- hello.py
+++ hello.py
@@ -1,16 +1,20 @@
 import os
 import sys
 
-print("")
-print("##       ####  ######  ######## ##    ##  ######  ########") 
-print("##        ##  ##    ## ##       ###   ## ##    ## ##      ") 
-print("##        ##  ##       ##       ####  ## ##       ##      ") 
-print("##        ##  ##       ######   ## ## ##  ######  ######  ") 
-print("##        ##  ##       ##       ##  ####       ## ##      ") 
-print("##        ##  ##    ## ##       ##   ### ##    ## ##      ") 
-print("######## ####  ######  ######## ##    ##  ######  ########") 
-print("")
-print("Hello, Creative Commons and Open Source")
+# Set 1 to enable the welcome print
+welcome_print = 1
+
+if welcome_print:
+    print("")
+    print("##       ####  ######  ######## ##    ##  ######  ########") 
+    print("##        ##  ##    ## ##       ###   ## ##    ## ##      ") 
+    print("##        ##  ##       ##       ####  ## ##       ##      ") 
+    print("##        ##  ##       ######   ## ## ##  ######  ######  ") 
+    print("##        ##  ##       ##       ##  ####       ## ##      ") 
+    print("##        ##  ##    ## ##       ##   ### ##    ## ##      ") 
+    print("######## ####  ######  ######## ##    ##  ######  ########") 
+    print("")
+    print("Hello, Creative Commons and Open Source\n")
 
 # define the color_print
 def color_print(color, text):
@@ -41,16 +45,50 @@ license_type = 'Unknown License'
 
 if 'MIT License' in content:
     license_type = 'MIT'
-elif 'GNU GENERAL PUBLIC LICENSE' in content:
-    if 'Version 2' in content:
-        license_type = 'GNU GPL-2.0'
-    elif 'Version 3' in content:
-        license_type = 'GNU GPL-3.0'
+elif 'BSD' in content:
+    license_type = 'BSD '
+    if '2-Clause' in content:
+        license_type += '2-Clause'
+    elif '3-Clause' in content:
+        license_type += '3-Clause'
 elif 'Apache License' in content:
-    license_type = 'Apache'
+    license_type = 'Apache-'
+    if 'Version 2' in content:
+        license_type += '2.0'
+elif 'Mozilla Public License' in content:
+    license_type = 'MPL-'
+    if 'Version 2' in content:
+        license_type += '2.0'
+elif 'Eclipse Public License' in content:
+    license_type = 'EPL-'
+    if 'Version 2' in content:
+        license_type += '2.0'
+elif 'GNU' in content:
+    license_type = 'GNU '
+    if 'GNU AFFERO GENERAL PUBLIC LICENSE' in content:
+        license_type += 'AGPL-'
+        if 'Version 3' in content:
+            license_type += '3.0'
+    elif 'GNU LESSER GENERAL PUBLIC LICENSE':
+        license_type += 'LGPL-'
+        if 'Version 2.1' in content:
+            license_type += '2.1'
+        elif 'Version 3' in content:
+            license_type += '3.0'
+    elif 'GNU GENERAL PUBLIC LICENSE' in content:
+        license_type += 'GPL-'
+        if 'Version 2' in content:
+            license_type += '2.0'
+        elif 'Version 3' in content:
+            license_type += '3.0'
 elif 'This is free and unencumbered software' in content:
     # which means no rights reserved, AKA Public Domain
     license_type = 'Unlicense'
+elif 'Creative Commons' in content:
+    if 'CC0 1.0 Universal' in content:
+        license_type = 'CC0-1.0'
+    elif 'Attribution 4.0 International' in content:
+        license_type = 'CC-BY-4.0'
 else:
     color_print('yellow', 'Unknown License')
     sys.exit()
